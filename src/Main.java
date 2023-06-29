2ee73b19c7f28f233cabaf9243ef337621d862bf
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-06-29 15:04:50 -0300

    Exercícios de Vetores (1)*

--- src/Main.java
+++ src/Main.java
@@ -7,27 +7,6 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        //Ler um número positivo N e depois N números inteiros, armazenando em um vetor.
-        //Em seguida, mostrar na telas os números negativos lidos
-        Locale.setDefault(Locale.US);
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Digite um número: ");
-        int n = scan.nextInt();
-        int[] vetor = new int[n];
 
-        for(int i = 0; i < vetor.length; i++) {
-            scan.nextLine();
-            System.out.println("\nDigite um número: ");
-            int numero = scan.nextInt();
-            vetor[i] = numero;
-        }
-
-        System.out.println("Números negativos: ");
-        for(int i = 0; i < vetor.length; i++) {
-            if(vetor[i] < 0) {
-                System.out.println(vetor[i]);
-            }
-        }
-        scan.close();
-        }
     }
+}
