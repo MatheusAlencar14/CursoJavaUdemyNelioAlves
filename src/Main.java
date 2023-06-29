d51a060c15102a095bd2718dbb2c22a6703f4e09
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-06-29 14:32:25 -0300

    Trabalhando com vetores

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        int n = scan.nextInt();
+        Products[] vetor = new Products[n];
+
+        for(int i = 0; i < vetor.length; i++) {
+            scan.nextLine();
+            System.out.println("\nDigite o nome do produto: ");
+            String name = scan.nextLine();
+            System.out.println("Digite o preço do produto: ");
+            double price = scan.nextDouble();
+            vetor[i] = new Products(name, price);
+        }
+
+        double soma = 0;
+
+        for(int i = 0; i < vetor.length; i++) {
+            soma += vetor[i].getPrice();
+        }
+
+        double media = soma / n;
+
+        System.out.printf("\nA média do preço dos produtos é: %.2f%n", media);
+
+        scan.close();
         }
     }
