8eae4bd245a2c1d3ab3842da708ee65484931b1e
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-06-30 11:30:17 -0300

    Entendendo como utilizar o for each

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        String[] vet = new String[] {"Maria", "João", "Matheus"};
+
+        for (int i = 0; i < vet.length; i++) {
+            System.out.println(vet[i]);
+        }
+
+        System.out.println("-------------");
+
+        for (String nome : vet) {
+            System.out.println(nome);
+        }
     }
 }
