42656c6ad9b98298c3f79294f17cae9801219bd1
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-06-30 16:37:23 -0300

    Exercício de Array e Listas (1)

---
+++ src/application/ProgramExercicioFuncionario.java
@@ -0,0 +1,59 @@
+package application;
+
+import entities.ExercicioFuncionario;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramExercicioFuncionario {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+        List<ExercicioFuncionario> func = new ArrayList<>();
+
+        System.out.println("Deseja cadastrar quantos funcionários?");
+        int num = scan.nextInt();
+        for (int i = 0; i < num; i++) {
+            System.out.println("Funcionário #" + (i + 1) + ": ");
+            System.out.print("ID: ");
+            Integer id = scan.nextInt();
+            scan.nextLine();
+            System.out.print("Nome: ");
+            String nome = scan.nextLine();
+            System.out.print("Salário: ");
+            Double salario = scan.nextDouble();
+            ExercicioFuncionario funcionario = new ExercicioFuncionario(id, nome, salario);
+            func.add(funcionario);
+        }
+
+        System.out.println();
+        System.out.println("Lista de funcionários: ");
+        for (ExercicioFuncionario x : func) {
+            System.out.println(x);
+        }
+
+        System.out.println();
+        System.out.print("Digite o ID do funcionário que receberá o aumento: ");
+        int idAumento = scan.nextInt();
+        ExercicioFuncionario f1 = func.stream().filter(x -> x.getId() == idAumento).findFirst().orElse(null);
+        System.out.println(f1);
+        if(f1 != null) {
+            System.out.print("Digite o percentual de aumento: ");
+            double porcentagem = scan.nextDouble();
+            f1.aumentoSalario(porcentagem);
+        } else {
+            System.out.println("Esse ID é inexistente!");
+        }
+
+        System.out.println();
+        System.out.println("Lista de funcionários: ");
+        for (ExercicioFuncionario x : func) {
+            System.out.println(x);
+        }
+        scan.close();
+    }
+}
---
+++ src/entities/ExercicioFuncionario.java
@@ -0,0 +1,42 @@
+package entities;
+
+public class ExercicioFuncionario {
+    private Integer id;
+    private String nome;
+    private Double salario;
+
+    public ExercicioFuncionario(int id, String nome, double salario) {
+        this.id = id;
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+
+    public void aumentoSalario (double porcentagem) {
+        salario += salario * porcentagem / 100;
+    }
+
+    @Override
+    public String toString() {
+        return id + ", " + nome + ", R$ " + salario;
+    }
+}
