28be9dd7f2772ea216105eecd2f4fd7ff6003b2b
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-07-03 10:23:15 -0300

    Entendendo funcionamento de Matrizes: Exercício

---
+++ src/exercicios/ExercicioMatriz1.java
@@ -0,0 +1,56 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class ExercicioMatriz1 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int m, n, x;
+        System.out.println("Digite o número de linhas: ");
+        m = scan.nextInt();
+        System.out.println("Digite o número de colunas: ");
+        n = scan.nextInt();
+        int [][] matriz = new int[m][n];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = scan.nextInt();
+            }
+        }
+        System.out.println();
+        System.out.println("Digite o número a ser procurado: ");
+        x = scan.nextInt();
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] == x) {
+                    System.out.println("Posição do número " + x + ": Linha: " + i + ", Coluna: " + j);
+                }
+            }
+        }
+
+        System.out.println();
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] == x) {
+                    if (i > 0) {
+                        System.out.println("Acima: " + matriz[i - 1][j]);
+                    }
+                    if (j > 0) {
+                        System.out.println("Esquerda: " + matriz [i][j - 1]);
+                    }
+                    if (j < matriz.length - 1) {
+                        System.out.println("Direita: " + matriz[i][j + 1]);
+                    }
+                    if (i < matriz.length - 1) {
+                        System.out.println("Abaixo: " + matriz[i + 1][j]);
+                    }
+                }
+            }
+        }
+
+        scan.close();
+    }
+}
